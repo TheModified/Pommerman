28a76534f70b2cde56aa7e2da4986f6ffbc447ac
Meij,Jop J. van der <441227@student.fontys.nl>
2020-12-30 18:47:57 +0100

    Working on lesson 2

--- serpentine/my_agent.py
+++ serpentine/my_agent.py
@@ -1,5 +1,6 @@
+import numpy as np
 from pommerman import characters
-from pommerman.constants import Action
+from pommerman.constants import Action, Item
 from pommerman.agents import BaseAgent
 
 
@@ -13,8 +14,38 @@ class MyAgent(BaseAgent):
     def act(self, obs, action_space):
         # Main event that is being called on every turn.
         if len(self.queue) == 0:
-            self.queue.append(Action.Right)
-            self.queue.append(Action.Down)
-            self.queue.append(Action.Left)
-            self.queue.append(Action.Up)
+            my_location = obs['position']
+            board = obs['board']
+            if self.check_left(board, my_location):
+                self.queue.append(Action.Left)
+            if self.check_right(board, my_location):
+                self.queue.append(Action.Right)
+            if self.check_up(board, my_location):
+                self.queue.append(Action.Up)
+            if self.check_down(board, my_location):
+                self.queue.append(Action.Down)
         return self.queue.pop()
+
+    def check_left(self, board, my_location):
+        row, col = my_location
+        if board[row, max(col - 1, 0)] == Item.Passage.value:
+            return True
+        return False
+
+    def check_up(self, board, my_location):
+        row, col = my_location
+        if board[max(col - 1, 0), row] == Item.Passage.value:
+            return True
+        return False
+
+    def check_right(self, board, my_location):
+        row, col = my_location
+        if board[row, min(col + 1, 7)] == Item.Passage.value:
+            return True
+        return False
+
+    def check_down(self, board, my_location):
+        row, col = my_location
+        if board[min(col + 1, 7), row] == Item.Passage.value:
+            return True
+        return False
