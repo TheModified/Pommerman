1376a9ff4769039904b722c011e21ffb0ade9450
Meij,Jop J. van der <441227@student.fontys.nl>
2021-01-09 14:18:09 +0100

    Finished lesson 2

--- serpentine/my_agent.py
+++ serpentine/my_agent.py
@@ -16,35 +16,52 @@ class MyAgent(BaseAgent):
         if len(self.queue) == 0:
             my_location = obs['position']
             board = obs['board']
-            if self.check_left(board, my_location):
+            if self.check_if_left_is_clear_space(board, my_location):
                 self.queue.append(Action.Left)
-            if self.check_right(board, my_location):
-                self.queue.append(Action.Right)
-            if self.check_up(board, my_location):
+            if self.check_if_up_is_clear_space(board, my_location):
                 self.queue.append(Action.Up)
-            if self.check_down(board, my_location):
+            if self.check_if_right_is_clear_space(board, my_location):
+                self.queue.append(Action.Right)
+            if self.check_if_down_is_clear_space(board, my_location):
                 self.queue.append(Action.Down)
+
+            if not self.queue:
+                self.queue.append(Action.Stop)
+
         return self.queue.pop()
 
-    def check_left(self, board, my_location):
+    def check_if_left_is_clear_space(self, board: np.array, my_location: tuple) -> bool:
+        """
+        Checks if we can move to the left. Returns true if possible.
+        """
         row, col = my_location
         if board[row, max(col - 1, 0)] == Item.Passage.value:
             return True
         return False
 
-    def check_up(self, board, my_location):
+    def check_if_up_is_clear_space(self, board, my_location):
+        """
+        Checks if we can move up. Returns true if possible.
+        """
         row, col = my_location
         if board[max(col - 1, 0), row] == Item.Passage.value:
             return True
         return False
 
-    def check_right(self, board, my_location):
+    def check_if_right_is_clear_space(self, board, my_location):
+        """
+        Checks if we can move to the right. Returns true if possible.
+        """
         row, col = my_location
         if board[row, min(col + 1, 7)] == Item.Passage.value:
             return True
         return False
 
-    def check_down(self, board, my_location):
+    def check_if_down_is_clear_space(self, board, my_location):
+
+        """
+        Checks if we can move down. Returns true if possible.
+        """
         row, col = my_location
         if board[min(col + 1, 7), row] == Item.Passage.value:
             return True
